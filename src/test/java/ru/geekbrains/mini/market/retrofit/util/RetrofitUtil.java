e877f80f8ccda36c7d037979e1de2eb1c1d45001
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-04-28 08:17:36 +0300

    retrofit

--- src/test/java/ru/geekbrains/mini/market/retrofit/util/RetrofitUtil.java
+++ src/test/java/ru/geekbrains/mini/market/retrofit/util/RetrofitUtil.java
@@ -5,7 +5,6 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 import ru.geekbrains.mini.market.retrofit.endpoints.CategoryService;
 import ru.geekbrains.mini.market.retrofit.endpoints.ProductService;
 
-
 import static ru.geekbrains.mini.market.retrofit.config.MiniMarketConfig.miniMarketConfig;
 
 //для прокидывания параметров подключения
@@ -16,19 +15,17 @@ public class RetrofitUtil {
                 .baseUrl(miniMarketConfig.baseURL())
                 .addConverterFactory(JacksonConverterFactory.create())
 //                .client(new OkHttpClient.Builder()
- //                       .addInterceptor(new HttpLoggingInterceptor(new PrettyLogger()).setLevel(HttpLoggingInterceptor.Level.BODY))
+                //                       .addInterceptor(new HttpLoggingInterceptor(new PrettyLogger()).setLevel(HttpLoggingInterceptor.Level.BODY))
 //                        .addInterceptor(new AllureOkHttp3())
 //                        .build())
                 .build();
-
+    }
         public static CategoryService getCategoryService()                              //регистрируем сервис для прокидывания url из categoryServise
         {
             return getRetrofit().create(CategoryService.class);                         //создали объект класса - который имплементирует CategoryService (теперь им можно пользоваться как эндпоинтом)
         }
 
-        public static ProductService getProductService()                                //регистрируем сервис для прокидывания url из productServise
-        {
-            return getRetrofit().create(ProductService.class);
+        public static ProductService getProductService() {                              //регистрируем сервис для прокидывания url из productServise
+             return getRetrofit().create(ProductService.class);
         }
     }
-}
